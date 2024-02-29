214befefaff3e3ee86e3522fb466c83141ce7343
PhuocTaii <tai.ngo1773@gmail.com>
2024-02-29 19:04:23 +0700

    add close button for sign-in page

--- app/src/main/java/com/deadlineshooters/yudemy/activities/SignInActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SignInActivity.kt
@@ -2,9 +2,11 @@ package com.deadlineshooters.yudemy.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.deadlineshooters.yudemy.R
 
 class SignInActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
