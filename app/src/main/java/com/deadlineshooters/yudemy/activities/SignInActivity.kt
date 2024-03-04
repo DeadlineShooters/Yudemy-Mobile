abfc2db976312b1fdb7cc6f67d2c16e13fa2df31
PhuocTaii <tai.ngo1773@gmail.com>
2024-03-04 16:46:23 +0700

    update UI
    change UI theme, update logic for login UI

--- app/src/main/java/com/deadlineshooters/yudemy/activities/SignInActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SignInActivity.kt
@@ -1,14 +1,27 @@
 package com.deadlineshooters.yudemy.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.deadlineshooters.yudemy.R
 
 class SignInActivity : AppCompatActivity() {
-
+    private var signupHref: TextView? = null
+    private var closeBtn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        signupHref = findViewById(R.id.signupHref)
+        closeBtn = findViewById(R.id.closeBtn)
+
+        signupHref?.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent);
+        }
+
+
     }
 }
