4007e1b5b825bf44de03044fa58068885d6937df
PhuocTaii <tai.ngo1773@gmail.com>
2024-03-04 17:05:44 +0700

    update UI
    delete close button, add logic to start sign in activity

--- app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpActivity.kt
@@ -1,12 +1,21 @@
 package com.deadlineshooters.yudemy.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.deadlineshooters.yudemy.R
 
 class SignUpActivity : BaseActivity() {
+    private var signinHref: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        signinHref = findViewById(R.id.signinHref)
+        signinHref!!.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
