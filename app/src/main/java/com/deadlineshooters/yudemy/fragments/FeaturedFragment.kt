b7b37b85a436a93c827cef177d2af1dc9c8315ca
Ngoc <dungtuyetngoc@gmail.com>
2024-03-09 11:34:46 +0700

    added course feedback popup Æbut still got áºeror loading the vectors

--- app/src/main/java/com/deadlineshooters/yudemy/fragments/FeaturedFragment.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/fragments/FeaturedFragment.kt
@@ -1,11 +1,16 @@
 package com.deadlineshooters.yudemy.fragments
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.deadlineshooters.yudemy.R
 import com.deadlineshooters.yudemy.activities.CourseDetailActivity
 
@@ -30,6 +35,27 @@ class FeaturedFragment : Fragment(), View.OnClickListener {
         val view = inflater.inflate(R.layout.fragment_featured, container, false)
 
         view.findViewById<View>(R.id.btn_courseDetail).setOnClickListener(this)
+
+        // test code for feedback popup
+        val btnPopUpFeedback = view.findViewById<Button>(R.id.btn_popUpFeedback)
+        btnPopUpFeedback.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(true)
+            dialog.setContentView(R.layout.course_feedback_popup_layout) // replace 'your_layout' with the name of your layout file
+
+            dialog.setCancelable(false) // Prevent the dialog from being dismissed when the user touches outside the dialog
+
+            val closeButton = dialog.findViewById<ImageView>(R.id.iv_exit)
+            closeButton.setOnClickListener {
+                // Handle close button click here
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
+
+
         return view
     }
     override fun onClick(v: View?) {
