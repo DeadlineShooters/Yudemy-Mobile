5b4890674a1f5d01a16e1ac77382e4d20556b422
PhuocTaii <tai.ngo1773@gmail.com>
2024-03-11 14:41:19 +0700

    finish sign up ui

--- app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpActivity.kt
@@ -3,18 +3,31 @@ package com.deadlineshooters.yudemy.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.deadlineshooters.yudemy.R
 
 class SignUpActivity : BaseActivity() {
     private var signinHref: TextView? = null
+    private var signUpEmailBtn: Button? = null
+    private var signUpGoogleBtn: Button? = null
+    private var signUpFbBtn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         signinHref = findViewById(R.id.signinHref)
+        signUpEmailBtn = findViewById(R.id.signUpEmailBtn)
+        signUpGoogleBtn = findViewById(R.id.signUpGoogleBtn)
+        signUpFbBtn = findViewById(R.id.signUpFbBtn)
+
         signinHref!!.setOnClickListener{
             val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        signUpEmailBtn!!.setOnClickListener{
+            val intent = Intent(this, SignUpWithEmailActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpWithEmailActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpWithEmailActivity.kt
@@ -1,32 +1,55 @@
 package com.deadlineshooters.yudemy.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.deadlineshooters.yudemy.R
 import com.deadlineshooters.yudemy.databinding.ActivitySignUpWithEmailBinding
 
 class SignUpWithEmailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignUpWithEmailBinding
-
+//    private lateinit var binding: ActivitySignUpWithEmailBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivitySignUpWithEmailBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_sign_up_with_email)
+//        setupActionBar() // on back button click will navigate to the previous screen
+//    }
+//
+//    /**
+//     * replace default ActionBar with the < button
+//     */
+//    private fun setupActionBar() {
+//        setSupportActionBar(binding.toolbarSignUpWithEmailActivity)
+//
+//        val actionBar = supportActionBar
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true)
+//            actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_new_24)
+//        }
+//
+//        binding.toolbarSignUpWithEmailActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+    private var signUpBtn: Button? = null
+    private var signinHref: TextView? = null
+    private var email: EditText? = null
+    private var password: EditText? = null
+    private var name: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpWithEmailBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_sign_up_with_email)
-        setupActionBar() // on back button click will navigate to the previous screen
-    }
 
-    /**
-     * replace default ActionBar with the < button
-     */
-    private fun setupActionBar() {
-        setSupportActionBar(binding.toolbarSignUpWithEmailActivity)
+        signUpBtn = findViewById(R.id.signUpBtn)
+        signinHref = findViewById(R.id.signinHref)
+        name = findViewById(R.id.nameInput)
+        email = findViewById(R.id.mailInput)
+        password = findViewById(R.id.passInput)
 
-        val actionBar = supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_new_24)
+        signinHref!!.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
         }
-
-        binding.toolbarSignUpWithEmailActivity.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 }
