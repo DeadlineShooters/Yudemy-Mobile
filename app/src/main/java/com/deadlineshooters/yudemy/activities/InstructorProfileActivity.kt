398b67401a104a614bb88a49f0e798becc83ab5c
PhuocTai <tai.ngo1773@gmail.com>
2024-03-13 12:07:50 +0700

    50% of instructor profile UI

---
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/InstructorProfileActivity.kt
@@ -0,0 +1,63 @@
+package com.deadlineshooters.yudemy.activities
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.deadlineshooters.yudemy.R
+import com.deadlineshooters.yudemy.models.Image
+import com.deadlineshooters.yudemy.models.Instructor
+
+class InstructorProfileActivity : BaseActivity() {
+    private lateinit var backBtn: Button
+    private lateinit var instructorName: TextView
+    private lateinit var instructorHeadline: TextView
+    private lateinit var instructorTotalStudents: TextView
+    private lateinit var instructorTotalReviews: TextView
+    private lateinit var instructorBio: TextView
+    private lateinit var showMoreBtn: TextView
+    private val dumpBio = "Hi, I'm Brad and I've taught web development to countless coworkers and held training sessions for fortune 100 companies.\n\n" +
+            "I also teach local night classes and run a somewhat popular web development tutorial YouTube channel named LearnWebCode.\n\n" +
+            "I'm a front-end developer, designer, and educator. I've been building user interfaces for over a decade for the world's largest brands, international technology leaders, and national political campaigns.\n\n" +
+            "I'm fortunate to enjoy the development work I do, but my true passion is helping people learn."
+    private val dumpInst = Instructor("123", "John", "Doe", "Web developer", 72087, 247011,dumpBio, Image("secure_url", "public_id"), "walletId", "walletName")
+    private var isExpanded = true
+    private lateinit var instructorCoursesOverview: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instructor_profile)
+
+        backBtn = findViewById(R.id.backBtn5)
+        instructorName = findViewById(R.id.instructorName)
+        instructorHeadline = findViewById(R.id.instructorHeadline)
+        instructorTotalStudents = findViewById(R.id.instructorTotalStudents)
+        instructorTotalReviews = findViewById(R.id.instructorTotalReviews)
+        instructorBio = findViewById(R.id.instructorBio)
+        showMoreBtn = findViewById(R.id.showMoreBtn)
+        instructorCoursesOverview = findViewById(R.id.instructorCoursesOverview)
+
+        instructorName.text = "${dumpInst.firstName} ${dumpInst.lastName}"
+        instructorHeadline.text = dumpInst.headline
+        instructorTotalStudents.text = dumpInst.totalStudents.formatThousands()
+        instructorTotalReviews.text = dumpInst.totalReviews.formatThousands()
+        instructorBio.text = dumpInst.bio
+
+        showMoreBtn.setOnClickListener{
+            if(isExpanded){
+                instructorBio.maxLines = Integer.MAX_VALUE
+                showMoreBtn.text = "Show less"
+            } else {
+                instructorBio.maxLines = 7
+                showMoreBtn.text = "Show more"
+            }
+            isExpanded = !isExpanded
+        }
+
+        //TODO: get the number of courses taught by the instructor from db
+        instructorCoursesOverview.text = "My course (9)"
+        
+
+    }
+}
