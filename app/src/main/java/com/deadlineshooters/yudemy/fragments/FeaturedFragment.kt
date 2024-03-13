875402f81dad16c851e3d5072e814e28edc00548
ItsMeNozo <dungtuyetngoc@gmail.com>
2024-03-13 22:32:40 +0700

    fixed imageview can't render in course detail

--- app/src/main/java/com/deadlineshooters/yudemy/fragments/FeaturedFragment.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/fragments/FeaturedFragment.kt
@@ -36,13 +36,43 @@ class FeaturedFragment : Fragment(), View.OnClickListener {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_featured, container, false)
 
+        view.findViewById<View>(R.id.btn_courseDetail).setOnClickListener(this)
+
+        // test code for feedback popup
+        val btnPopUpFeedback = view.findViewById<Button>(R.id.btn_popUpFeedback)
+        btnPopUpFeedback.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(true)
+            dialog.setContentView(R.layout.course_feedback_popup_layout) // replace 'your_layout' with the name of your layout file
+
+            dialog.setCancelable(false) // Prevent the dialog from being dismissed when the user touches outside the dialog
+
+            val closeButton = dialog.findViewById<ImageView>(R.id.iv_exit)
+            closeButton.setOnClickListener {
+                // Handle close button click here
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
+        val btnGoToInstructor = view.findViewById<Button>(R.id.btn_instructor)
+
+        btnGoToInstructor.setOnClickListener {
+            val intent = Intent(context, InstructorMainActivity::class.java)
+            startActivity(intent)
+        }
 
 
         return view
     }
     override fun onClick(v: View?) {
         when (v?.id) {
-//
+            R.id.btn_courseDetail -> {
+                // Start CourseDetailActivity
+                val intent = Intent(activity, CourseDetailActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 
