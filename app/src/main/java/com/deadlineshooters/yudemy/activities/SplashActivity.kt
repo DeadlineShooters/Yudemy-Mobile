cb327116d753a13b3d7ecd5d1a5335806ab3ba3b
liwjln <phamtrantuantu17@gmail.com>
2024-03-18 21:58:28 +0700

    Fixed wishlist ListView

--- app/src/main/java/com/deadlineshooters/yudemy/activities/SplashActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SplashActivity.kt
@@ -27,7 +27,7 @@ class SplashActivity : AppCompatActivity() {
 //                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
 //            } else {
                 // Start the Intro Activity
-                startActivity(Intent(this@SplashActivity, SignInActivity::class.java))
+                startActivity(Intent(this@SplashActivity, StudentMainActivity::class.java))
 //            }
             finish() // Call this when your activity is done and should be closed.
         }, 2500) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
