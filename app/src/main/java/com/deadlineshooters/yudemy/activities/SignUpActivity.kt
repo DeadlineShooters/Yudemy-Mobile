8c50e981e22e3dd856a861afecbe62e8009f70c0
PhuocTaii <tai.ngo1773@gmail.com>
2024-03-23 22:30:22 +0700

    sign up with google

--- app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpActivity.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/activities/SignUpActivity.kt
@@ -3,27 +3,29 @@ package com.deadlineshooters.yudemy.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.deadlineshooters.yudemy.R
-import com.deadlineshooters.yudemy.repositories.AuthenticationRepository
-import com.deadlineshooters.yudemy.utils.Constants.WEB_CLIENT_ID
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.Firebase
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
 
 class SignUpActivity : BaseActivity() {
     private var signinHref: TextView? = null
     private var signUpEmailBtn: Button? = null
     private var signUpGoogleBtn: Button? = null
     private var signUpFbBtn: Button? = null
-    private val googleSignInRC = 9001
+    private val RC_SIGN_IN = 9001
     private lateinit var googleSignInClient: GoogleSignInClient
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +37,12 @@ class SignUpActivity : BaseActivity() {
         signUpFbBtn = findViewById(R.id.signUpFbBtn)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(WEB_CLIENT_ID)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
+        Log.d("gso", gso.toString())
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
 
         signinHref!!.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
@@ -52,31 +56,41 @@ class SignUpActivity : BaseActivity() {
 
         signUpGoogleBtn!!.setOnClickListener {
             val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, googleSignInRC)
+            startActivityForResult(signInIntent, RC_SIGN_IN)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when (requestCode) {
-            googleSignInRC -> {
-                try {
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-                    val account = task.result
-                    AuthenticationRepository().firebaseAuthWithGoogle(account!!.idToken!!){
-                        if(it != null){
-                            startActivity(Intent(this, StudentMainActivity::class.java))
-                        } else {
-                            Toast.makeText(this, "Failed to sign in with Google", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.e("GoogleSignIn", "signInResult:failed code=" + e.statusCode)
+                // Hiển thị thông báo lỗi cho người dùng nếu cần
+                Toast.makeText(this, "Google sign in failed: " + e.statusCode, Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    val intent = Intent(this, StudentMainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                }
+            }
+    }
 }
