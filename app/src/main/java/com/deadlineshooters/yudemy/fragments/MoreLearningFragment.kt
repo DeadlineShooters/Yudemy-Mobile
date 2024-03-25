e03b0cafc849eb8fa2619220650d4d1ddcd7acb7
Van Tran <thanhvan26823@gmail.com>
2024-03-26 01:30:42 +0700

    show dialog of about this course with description

--- app/src/main/java/com/deadlineshooters/yudemy/fragments/MoreLearningFragment.kt
+++ app/src/main/java/com/deadlineshooters/yudemy/fragments/MoreLearningFragment.kt
@@ -1,13 +1,22 @@
 package com.deadlineshooters.yudemy.fragments
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.deadlineshooters.yudemy.R
+import com.deadlineshooters.yudemy.databinding.FragmentMoreLearningBinding
 import com.deadlineshooters.yudemy.dialogs.QADialog
+import com.deadlineshooters.yudemy.viewmodels.CourseViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,14 +31,20 @@ class MoreLearningFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var courseId: String? = null
     private lateinit var qa: TextView
+    private lateinit var binding: FragmentMoreLearningBinding
+    private lateinit var aboutDialog: BottomSheetDialog
+    private lateinit var courseViewModel: CourseViewModel
 
     val title = "More"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            courseId = it.getString(ARG_COURSE_ID)
+//            courseId = it.getString(ARG_COURSE_ID) TODO: Uncomment this line
+            courseId = "2tNxr8j5FosEueZrL3wH"
         }
+
+        courseViewModel = ViewModelProvider(requireActivity())[CourseViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -37,7 +52,9 @@ class MoreLearningFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more_learning, container, false)
+        binding = FragmentMoreLearningBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,9 +65,35 @@ class MoreLearningFragment : Fragment() {
             val qaDialog = QADialog()
             qaDialog.show(parentFragmentManager, "QADialog")
         }
+
+        aboutDialog = createAboutDialog()
+        binding.navAbout.setOnClickListener {
+            aboutDialog.show()
+
+            courseViewModel.learningCourse.observe(viewLifecycleOwner, Observer{
+                aboutDialog.findViewById<TextView>(R.id.contentAboutCourse)!!.text = it?.description
+            })
+        }
     }
 
+    private fun createAboutDialog(): BottomSheetDialog {
+        val dialog = BottomSheetDialog(requireContext(), R.style.StandardBottomSheetDialog)
+        val bottomSheet = layoutInflater.inflate(R.layout.dialog_about_this_course, null)
 
+        bottomSheet.findViewById<TextView>(R.id.closeDialogAbout).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.setContentView(bottomSheet)
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        val layout = dialog.findViewById<CoordinatorLayout>(R.id.aboutBottomSheetLayout)
+        if (layout != null) {
+            layout.minimumHeight = Resources.getSystem().displayMetrics.heightPixels
+        }
+
+        return dialog
+    }
 
 
     companion object {
